efbd0d8c426aaf0dc88b52423b88bcbe1509e774
alexlittle <alex@alexlittle.net>
2015-01-11 22:28:28 +0100

    tag view

--- photo/urls.py
+++ photo/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     url(r'^scan/$', 'photo.views.scan_folder', name="photo_scan"),
     url(r'^location/(?P<location_id>\d+)$', 'photo.views.location_view', name="photo_location"),
     url(r'^thumbnail/(?P<photo_id>\d+)$', 'photo.views.thumbnail_view', name="photo_thumbnail"),
+    url(r'^tag/(?P<tag_id>\d+)$', 'photo.views.tag_view', name="photo_tag"),
 )
--- photo/views.py
+++ photo/views.py
@@ -24,17 +24,28 @@ def home_view(request):
     
 def location_view(request, location_id):
     location = Location.objects.get(pk=location_id)
+    photos = Photo.objects.filter(location=location).order_by('date')
+    for p in photos:
+        p.tags = Tag.objects.filter(phototag__photo=p)
     
-    photos = Photo.objects.filter(location=location)
-    return render_to_response('photo/location.html',
-                               {'location': location,
+    return render_to_response('photo/set.html',
+                               {'title': location.name,
                                 'photos': photos},
                               context_instance=RequestContext(request))
   
+def tag_view(request, tag_id):
+    tag = Tag.objects.get(pk=tag_id)
+    photos = Photo.objects.filter(phototag__tag=tag).order_by('date')
+    for p in photos:
+        p.tags = Tag.objects.filter(phototag__photo=p)
+    return render_to_response('photo/set.html',
+                               {'title': tag.name,
+                                'photos': photos},
+                              context_instance=RequestContext(request))
+    
 def thumbnail_view(request, photo_id):
     photo = Photo.objects.get(pk=photo_id)
     image = settings.PHOTO_ROOT + photo.location.name + photo.file
-    print image
     im = Image.open(image)
     im.thumbnail(size=(200,200))
     response = HttpResponse(content_type="image/jpg")
