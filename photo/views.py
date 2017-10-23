8dcb8f73b4f01517084b64c134c389e6c6296835
Alex Little <alex@digital-campus.org>
2017-10-23 19:16:22 +0300

    update for django 1.11.x

--- photo/views.py
+++ photo/views.py
@@ -12,7 +12,7 @@ from PIL.ExifTags import TAGS
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect, Http404, HttpResponse
-from django.shortcuts import render,render_to_response
+from django.shortcuts import render
 from django.template import RequestContext
 from django.utils import timezone
 from django.utils.dateparse import parse_datetime
@@ -26,9 +26,8 @@ from photo.models import Location, Photo, PhotoTag, Tag
 
 def home_view(request):
     locations = Location.objects.all().order_by('-name')
-    return render_to_response('photo/home.html',
-                              {'locations': locations,},
-                              context_instance=RequestContext(request))
+    return render(request, 'photo/home.html',
+                              {'locations': locations,})
     
 def location_view(request, location_id):
     location = Location.objects.get(pk=location_id)
@@ -36,27 +35,24 @@ def location_view(request, location_id):
     for p in photos:
         p.tags = Tag.objects.filter(phototag__photo=p)
     
-    return render_to_response('photo/set.html',
+    return render(request, 'photo/set.html',
                                {'title': location.name,
-                                'photos': photos},
-                              context_instance=RequestContext(request))
+                                'photos': photos})
   
 def tag_view(request, tag_id):
     tag = Tag.objects.get(pk=tag_id)
     photos = Photo.objects.filter(phototag__tag=tag).order_by('date')
     for p in photos:
         p.tags = Tag.objects.filter(phototag__photo=p)
-    return render_to_response('photo/set.html',
+    return render(request, 'photo/set.html',
                                {'title': tag.name,
-                                'photos': photos},
-                              context_instance=RequestContext(request))
+                                'photos': photos})
  
 def cloud_view(request):
     tags = Tag.objects.all().order_by('name')
-    return render_to_response('photo/cloud.html',
+    return render(request,'photo/cloud.html',
                                {'title': _('Cloud'),
-                                'tags': tags},
-                              context_instance=RequestContext(request))
+                                'tags': tags})
        
 def thumbnail_view(request, photo_id):
     photo = Photo.objects.get(pk=photo_id)
