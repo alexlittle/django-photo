a4924d054e2244bd6144bf065af4ebb56d92df74
Alex Little <alex@digital-campus.org>
2017-11-20 21:15:03 +0200

    fixes #11

--- photo/admin.py
+++ photo/admin.py
@@ -5,21 +5,26 @@ from photo.models import Album, Photo, Tag, PhotoTag, TagCategory, ThumbnailCach
 
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ('name', 'title')
+    search_fields = ['name','title']
   
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('file', 'date', 'album')  
+    search_fields = ['file']
     
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'tagcategory')  
+    search_fields = ['name']
     
 class TagCategoryAdmin(admin.ModelAdmin):
     list_display = ('name', )  
-        
+    search_fields = ['name']
+       
 class PhotoTagAdmin(admin.ModelAdmin):
     list_display = ('photo', 'tag')      
 
 class ThumbnailCacheAdmin(admin.ModelAdmin):
     list_display = ('photo', 'size', 'image') 
+    search_fields = ['photo', 'image']
     
 admin.site.register(Album, AlbumAdmin)   
 admin.site.register(Photo, PhotoAdmin)  
