77cc2464eb04f35976ffb2c049d36245b8f927ac
Alex Little <alex@digital-campus.org>
2017-11-26 22:35:31 +0200

    13: Get uncategorized tags
    Task-Url: https://github.com/alexlittle/django-photo/issues/13

---
+++ photo/management/commands/uncategorised_tags.py
@@ -0,0 +1,33 @@
+
+"""
+Management command to get tags with no category set
+"""
+import os
+import time 
+import django.db.models
+
+from optparse import make_option
+
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand, CommandError
+
+from photo.models import PhotoTag, Tag
+from django.core.urlresolvers import reverse
+
+class Command(BaseCommand):
+    help = "Finds all uncategorised tags"
+
+
+    def add_arguments(self, parser):
+        pass
+        
+
+    def handle(self, *args, **options):
+        tags = Tag.objects.filter(tagcategory=None)
+        for t in tags:
+            print ("http://localhost:8000%s" % reverse('admin:photo_tag_change', args=(t.id, )))
+            
+            
+            
+        
