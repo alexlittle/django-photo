60cfb0fbc3e10c10a98f378db045ce69a54e826d
Alex Little <consult@alexlittle.net>
2023-03-11 19:09:16 +0200

    update bootstrap

--- photo/static/photo/functions.js
+++ photo/static/photo/functions.js
@@ -1,4 +1,9 @@
 
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+  event.preventDefault();
+  $(this).ekkoLightbox();
+});
+
 function selectall(){
   $(':checkbox').prop('checked', true);
 }
