e0582a384a8b70128eeaf9741ecc360f795485dd
Alex Little <consult@alexlittle.net>
2024-09-20 09:53:08 +0300

    improve flexibility for exporting tag photos

--- photo/management/commands/export_tag_photos.py
+++ photo/management/commands/export_tag_photos.py
@@ -4,6 +4,7 @@ from shutil import copy
 
 from django.conf import settings
 from django.core.management.base import BaseCommand
+from django.db.models import Count
 
 from photo.models import Photo, Tag
 
@@ -13,22 +14,25 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('tag')
+        parser.add_argument('export_path', nargs='?')
 
     def handle(self, *args, **options):
-        tag = Tag.objects.get(slug=options['tag'])
-        photos = Photo.objects.filter(phototag__tag=tag)
+        slug_list = options['tag'].split('+')
+        photos = Photo.objects.filter(phototag__tag__slug__in=slug_list) \
+            .annotate(count=Count('id')) \
+            .filter(count=len(slug_list))
 
-        export_path = os.path.join(settings.PHOTO_ROOT, 'export', tag.slug)
-        try:
-            os.makedirs(export_path)
-        except OSError:
-            print("couldn't create directory - maybe it already exists?")
+        if not options['export_path']:
+            export_path = os.path.join(settings.PHOTO_ROOT, 'export', options['tag'])
+            try:
+                os.makedirs(export_path)
+            except OSError:
+                print("couldn't create directory - maybe it already exists?")
+        else:
+            export_path = options['export_path']
 
         for photo in photos:
             file_to_copy = os.path.join(settings.PHOTO_ROOT,
                                         photo.album.name[1:],
                                         photo.file)
-            print(os.path.join(settings.PHOTO_ROOT,
-                               photo.album.name[1:],
-                               photo.file))
             copy(file_to_copy, export_path)
