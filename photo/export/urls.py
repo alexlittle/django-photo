a286619e6431b000d8975cafac4b755d710ddd2e
Alex Little <consult@alexlittle.net>
2025-04-01 12:09:29 +0300

    update for CBVs

--- photo/export/urls.py
+++ photo/export/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from photo.export import views as photo_export
 
 urlpatterns = [
-    path('<int:album_id>', photo_export.make_view_pdf, name="export_pdf"),
+    path('<int:album_id>', photo_export.MakeViewPDF.as_view(), name="export_pdf"),
     path('tag/<str:slug>', photo_export.tag_to_folder, name="export_tag_to_folder"),
 ]
--- photo/export/views.py
+++ photo/export/views.py
@@ -1,27 +1,41 @@
-
-from shutil import copy2
+import os
+import shutil
 
 from django.conf import settings
-from django.http import FileResponse, HttpResponseRedirect
 from django.urls import reverse
-from photo.models import Photo, Tag
+from django.views import View
+from django.http import FileResponse, Http404
+from django.shortcuts import get_object_or_404, redirect
+
+from photo.models import Photo, Tag, Album
 from photo.export import create_album
 
 
-def make_view_pdf(request, album_id):
-    album_url = create_album.make(album_id)
-    response = FileResponse(open(album_url, 'rb'),
-                            content_type='application/pdf')
-    response['Content-Disposition'] = "inline; filename=export-album" \
-        + str(album_id) + ".pdf"
-    return response
+class MakeViewPDF(View):
+    def get(self, request, album_id):
+        """Generate and return a PDF for the given album."""
+        album = get_object_or_404(Album, id=album_id)  # Ensure album exists
+        album_url = create_album.make(album_id)  # Generate PDF file path
+
+        if not os.path.exists(album_url):  # Handle missing file
+            raise Http404("PDF file not found")
+
+        response = FileResponse(open(album_url, 'rb'), content_type='application/pdf')
+        response['Content-Disposition'] = f'inline; filename=export-album{album_id}.pdf'
+        return response
 
 
-def tag_to_folder(request, slug):
-    dest = "/home/alex/Downloads/pauliina/"
-    tag = Tag.objects.get(slug=slug)
-    photos = Photo.objects.filter(phototag__tag=tag)
-    for photo in photos:
-        copy2(settings.PHOTO_ROOT + photo.album.name + photo.file, dest)
-    return HttpResponseRedirect(reverse('photo:tag_slug',
-                                        kwargs={'slug': slug}))
+class TagToFolderView(View):
+    def post(self, request, slug):
+        """Copy all photos tagged with `slug` into a folder."""
+        dest = os.path.join(settings.PHOTO_ROOT, "export", slug)
+        os.makedirs(dest, exist_ok=True)
+
+        tag = get_object_or_404(Tag, slug=slug)
+        photos = Photo.objects.filter(phototag__tag=tag)
+
+        for photo in photos:
+            src = os.path.join(settings.PHOTO_ROOT, photo.album.name.lstrip("/"), photo.file)
+            shutil.copy2(src, dest)  # Preserve metadata
+
+        return redirect(reverse('photo:tag_slug', kwargs={'slug': slug}))
