55252e1908f4f97288363fb0a38a3b36d381588b
Alex Little <consult@alexlittle.net>
2025-04-09 20:33:41 +0100

    update photo card layout

--- photo/static/photo/functions.js
+++ photo/static/photo/functions.js
@@ -7,3 +7,29 @@ function selectall(){
 function selectnone(){
   $(':checkbox').prop('checked', false);
 }
+
+// for highlighting photo background when selected
+$(document).ready(function() {
+  // On page load, find all checked checkboxes and add the 'selected' class
+  // to their closest '.photo-thumbnail-container' parent.
+  $('input[type="checkbox"]:checked').each(function() {
+    $(this).closest('.photo-thumbnail-container').addClass('selected');
+  });
+
+  // Click handler for toggling selection (same as before)
+  $('.photo-image').on('click', function(event) {
+    if ($(event.target).closest('a[data-toggle="lightbox"]').length) {
+      return;
+    }
+
+    const $photoImage = $(this);
+    const $container = $photoImage.closest('.photo-thumbnail-container');
+    const $checkbox = $container.find('input[type="checkbox"]'); // Find the checkbox within the container
+
+    if ($checkbox.length) {
+      $checkbox.prop('checked', !$checkbox.prop('checked'));
+      $container.toggleClass('selected', $checkbox.prop('checked'));
+      $checkbox.trigger('change');
+    }
+  });
+});
